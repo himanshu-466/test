767966d0ed808c3621fda0b40fcc5729112418e4
Himanshu Agrawal <himanshuagrawal466@gmail.com>
2023-10-30 21:21:05 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -12,6 +12,5 @@ root.render(
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log resulasdsadts (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// to log resulasdsadts (for example: reportWnalytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
